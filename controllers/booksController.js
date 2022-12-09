449d99d75656e04db9e3c52979684163a81a4231
Corinna Chang <corinnachang04@gmail.com>
2022-12-09 10:42:04 -0500

    Added object literal for exports in controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,31 +1,29 @@
 const Books = require("../models/booksmod");
 
-exports.getAllBooks = (req, res, next) => {
-  Books.find({}, (error, books) => {
-    if (error) next(error);
-    req.data = books;
-    next();
-  });
-};
-
-exports.getBookLoadPage = (req, res) => {
-  res.render("loadbook");
-};
-
-exports.saveBooks = (req, res) => {
-  let newBooks = new Books({
-    name: req.body.name,
-    page: req.body.page,
-    description: req.body.description,
-    img: req.body.img,
-  });
-  newBooks.save((error, result) => {
-    if (error) res.send(error);
-    console.log("Success")
-  });
-};
-
-exports.getbooks = (req, res, next) => {
+module.exports = {
+  getAllBooks: (req, res, next) => {
+    Books.find({}, (error, books) => {
+      if (error) next(error);
+      req.data = books;
+      next();
+    });
+  },
+  getBookLoadPage: (req, res) => {
+    res.render("loadbook");
+  },
+  saveBooks: (req, res) => {
+    let newBooks = new Books({
+      name: req.body.name,
+      page: req.body.page,
+      description: req.body.description,
+      img: req.body.img,
+    });
+    newBooks.save((error, result) => {
+      if (error) res.send(error);
+      console.log("Success")
+    });
+  },
+  getbooks: (req, res, next) => {
     let paramsName = req.params.bookID;
     console.log(req.params)
     Books.findOne({page: paramsName}, (error, book) => {
@@ -36,4 +34,5 @@ exports.getbooks = (req, res, next) => {
       });
     
     //res.render("bookpage");
-};
+  },
+}
